b1c0da437f6d4f04e602c5b4c84da6d8b61ad41d
syddl0 <137189866+shroqkf@users.noreply.github.com>
2025-04-01 11:20:11 +0900

    first file

---
+++ html/tailwind.config.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        primary: "#ffffff",
+        secondary: "#ffed4a",
+        danger: "#e3342f",
+        clifford: "#da373d",
+        ocean: "#3742fa",
+        peace: "#a4b0be",
+      },
+    },
+  },
+};
